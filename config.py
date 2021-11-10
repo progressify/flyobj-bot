8bde4dd4c5375e2cd1c121f97708e7e0cab5cf44
progressify <a.porcelli90@gmail.com>
2021-11-10 10:20:55 +0100

    bozza rapidapi

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ MAPS_KEY = '***write here your key***'
 
 BASE_URL_RADAR = 'https://radar.freedar.uk/VirtualRadar/AircraftList.json'
 BASE_URL_MAPS = 'https://maps.googleapis.com/maps/api/staticmap'
+
+RAPID_API_BASE_URL = "https://adsbx-flight-sim-traffic.p.rapidapi.com/api/aircraft/json/lat/{lat}/lon/{lon}/dist/25/"
+RAPID_API_HOST = "adsbx-flight-sim-traffic.p.rapidapi.com"
+RADIP_API_KEY = "***write here your key***"
